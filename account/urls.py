fea947d591ce0a86792a35b8ebf30bec97db2296
mhmmdmous <muhammadmousaviar7@gmail.com>
2025-04-17 10:23:33 +0330

    M V1.3

--- account/urls.py
+++ account/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path("take-token/", TokenObtainPairView.as_view()),
     path("refresh-token/", TokenRefreshView.as_view()),
     path("verify-token/", TokenVerifyView.as_view()),
-
 ]
