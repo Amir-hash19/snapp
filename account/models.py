6ba2b3a27404b1e7654c7271c44d9dfc6839dc42
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-04-17 09:25:08 +0330

    add settings

--- account/models.py
+++ account/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Account(User):
+    wallet = models.PositiveIntegerField(default=1_000_000)
