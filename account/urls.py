999cb52e60f88079560b9f90293d0d6c1e4eed2c
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-04-17 09:56:34 +0330

    set tokens

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,11 @@
-from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView, TokenVerifyView
 from django.urls import path
 
 
 urlpatterns = [
-    path()
+    path("take-token/", TokenObtainPairView.as_view()),
+    path("refresh-token/", TokenRefreshView.as_view()),
+    path("verify-token/", TokenVerifyView.as_view()),
+
 ]
 
--- order/models.py
+++ order/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from account.models import Account
 
-# Create your models here.
+
+
+
+class Order(models.Model):
+    user = models.ForeignKey(to=Account, on_delete=models.CASCADE, verbose_name="User")
+    origin_lat = models.FloatField(verbose_name="Origin Latitude")
+    origin_lng = models.FloatField(verbose_name="Origin Longitude")
+    destination_lat = models.FloatField(verbose_name="Destination Latitude")
+    destination_lng = models.FloatField(verbose_name="Destination Longitude")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created At")
+
+  
+    def __str__(self):
+        return f"Order by {self.user} from ({self.origin_lat}, {self.origin_lng}) to ({self.destination_lat}, {self.destination_lng})"
